11e3b977cf84b1e17cb619ca1a023caa7ea0b1cf
Samer Mohamed <Samirsuraj@live.com>
2018-01-29 22:31:24 +0200

    add some html structure element to article#show

--- etchSketch/myjs.js
+++ etchSketch/myjs.js
@@ -1,7 +1,7 @@
 const pad = document.querySelector('#sketchPad');
 const Reset = document.querySelector('#reset');
 const gridSize = document.querySelector('#gridSize');
-const body=document.querySelector('#body')
+const body=document.querySelector('body')
 createGrid();
 Reset.addEventListener('click', createGrid);
 function createGrid() {
@@ -16,7 +16,7 @@ function createGrid() {
     box.setAttribute('class', 'lilbox');
     box.style.padding=(200/size).toString()+'px';
     //box.textContent = 'p';
-    box.addEventListener('mouseenter', (e) => {color='#'+(Math.random()*0xFFFFFF<<0).toString(16); e.target.style.backgroundColor = color; body.style.backgroundColor=color});
+    box.addEventListener('mouseenter', (e) => {color='#'+(Math.random()*0xFFFFFF<<0).toString(16); e.target.style.backgroundColor = color; body.style.backgroundImage="linear-gradient(to top right, white,"+color+")"});
     pad.appendChild(box);
   }
    pad.style.gridTemplateColumns= "auto ".repeat(size);
